df3d0a9468ca12d55c02e8d04dba4806e990f046
Aleksan <Dev.aleksan@gmail.com>
2021-11-24 14:36:48 +0300

    feat: add save city, icons, print Weather and axios

--- app/services/api.services.js
+++ app/services/api.services.js
@@ -1,30 +1,43 @@
-import https from 'https'
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js'
+import axios from 'axios'
 
-const getWeather = async (city = 'moscow') => {
-  const token = await getKeyValue(TOKEN_DICTIONARY.token)
-  const url = new URL('https://api.openweathermap.org/data/2.5/weather')
+const getIcon = (icon) => {
+  switch (icon.slice(0, -1)) {
+    case '01':
+      return '☀️'
+    case '02':
+      return '🌤️'
+    case '03':
+      return '☁️'
+    case '04':
+      return '☁️'
+    case '09':
+      return '🌧️'
+    case '10':
+      return '🌦️'
+    case '11':
+      return '🌩️'
+    case '13':
+      return '❄️'
+    case '50':
+      return '🌫️'
+  }
+}
+
+const getWeather = async (city) => {
+  const token = process.env.TOKEN ?? (await getKeyValue(TOKEN_DICTIONARY.token))
+  const url = 'https://api.openweathermap.org/data/2.5/weather'
   if (!token) {
     throw new Error('Не задан ключ API, задайте его через команду -t [API_KEY]')
   }
-  url.searchParams.append('q', city)
-  url.searchParams.append('appid', token)
-  url.searchParams.append('units', 'metric')
-  url.searchParams.append('lang', 'ru')
-
-  https.get(url, (response) => {
-    let result = ''
-    response.on('data', (chunk) => {
-      result += chunk
-    })
-
-    response.on('end', () => {
-      console.log(JSON.parse(result))
-    })
-    response.on('error', (err) => {
-      console.log(`Данные не получены, ошибка: ${err}`)
-    })
+  const { data } = await axios.get(url, {
+    params: {
+      q: city,
+      appid: token,
+      lang: 'ru',
+      units: 'metric',
+    },
   })
+  return data
 }
-
-export { getWeather }
+export { getWeather, getIcon }
--- app/weather.js
+++ app/weather.js
@@ -1,34 +1,83 @@
 #!/usr/bin/env node
-import { getArgs } from './helpers/args.js'
-import { printError, printHelp, printSuccess } from './services/log.services.js'
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js'
-import { getWeather } from './services/api.services.js'
 
+import { getArgs } from './helpers/args.js'
+import { printError, printHelp, printSuccess,printWeather } from './services/log.services.js'
+import {
+  getKeyValue,
+  saveKeyValue,
+  TOKEN_DICTIONARY,
+} from './services/storage.service.js'
+import { getIcon, getWeather } from './services/api.services.js'
+
+/**
+ * Сохранение токена
+ * @param token - токен строкой
+ */
 const saveToken = async (token) => {
   if (!token.length) {
-    printError('Не передан токен!')
+    printError('Не передан token')
     return
   }
   try {
     await saveKeyValue(TOKEN_DICTIONARY.token, token)
-    printSuccess('Token сохранēн')
+    printSuccess('Токен сохранён')
   } catch (e) {
     printError(e.message)
   }
 }
 
+/**
+ * Сохранение города
+ * @param city - токен строкой
+ */
+const saveCity = async (city) => {
+  if (!city.length) {
+    printError('Не передан город')
+    return
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.city, city)
+    printSuccess('Город сохранён')
+  } catch (e) {
+    printError(e.message)
+  }
+}
+
+/**
+ * Получение прогноза погоды
+ * @returns {Promise<any>}
+ */
+const getForecast = async () => {
+  try {
+    const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+    const weather = await getWeather(city);
+    printWeather(weather, getIcon(weather.weather[0].icon));
+  } catch (e) {
+    if (e?.response?.status == 404) {
+      printError('Неверно указан город');
+    } else if (e?.response?.status == 401) {
+      printError('Неверно указан токен');
+    } else {
+      printError(e.message);
+    }
+  }
+}
+
+/**
+ * Запуск приложения
+ */
 const initCLI = () => {
   const args = getArgs(process.argv)
   if (args.h) {
-    printHelp()
+    return printHelp()
   }
   if (args.c) {
-    printSuccess()
+    return saveCity(args.c)
   }
   if (args.t) {
     return saveToken(args.t)
   }
-  getWeather().then()
+  return getForecast()
 }
 
-await initCLI()
+initCLI()
